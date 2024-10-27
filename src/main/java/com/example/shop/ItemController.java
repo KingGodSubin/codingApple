6e4b1b080c01461dab937104f6ac3e078d41d978
soo982181 <soo982181@naver.com>
2024-10-27 20:49:10 +0900

    241027 study done

--- src/main/java/com/example/shop/ItemController.java
+++ src/main/java/com/example/shop/ItemController.java
@@ -2,15 +2,15 @@ package com.example.shop;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,7 +28,7 @@ public class ItemController {
     // 템플릿엔진 쓰면 서버/Database의 데이터를 HTML에 집어넣을 수 있음.
     // 이걸 API 라고 하는것 같음
     @GetMapping("/list")
-    public String list(Model model){
+    public String list(Model model) {
 
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
@@ -38,13 +38,14 @@ public class ItemController {
     }
 
     @GetMapping(value = "/write")
-    public String write(Item item){
+    public String write(Item item) {
         return "write";
     }
 
     @PostMapping(value = "/add")
     // @RequestParam Map formData => input 100개면 100개를 Map 자료형으로 담아줌
-    public String add(Item item){
+    // @ModelAttribute Item item => <input> 데이터들을 바로 object로 변환하려면
+    public String addPost(Item item) {
         itemRepository.save(item);
 //        var test = new HashMap<>();
 //        test.put("name", "kim");
@@ -53,11 +54,29 @@ public class ItemController {
         // 특정페이지로 돌아가게 만들 수 있음(redirect)
         return "redirect:/list";
     }
+
+    // URL 파라미터 문법쓰면 비슷한 URL의 API 여러개 만들 필요 없음
+    @GetMapping(value = "/detail/{itemId}")
+    public String detail(Model model, @PathVariable Long itemId) {
+
+        // Q 왜 Optional 써야함?
+        // JPA findById() 만든 사람이 쓰라는데요
+        Optional<Item> result = itemRepository.findById(itemId);
+        if (result.isPresent()) {
+            model.addAttribute("result", result.get());
+            return "detail";
+        } else {
+            return "redirect:/list";
+        }
+
+    }
+
+
     // HTML에 서버데이터 넣어서 보내주려면
     // 1. Model model 추가
     // 2. model.addAttribute(작명, 데이터)
     // 3. th:text="${작명}"
-    
+
     // JPA로 데이터 입출력하려면 3-step
     // 1. repository 만들기
     // 2. 원하는 클래스에 repository 등록
